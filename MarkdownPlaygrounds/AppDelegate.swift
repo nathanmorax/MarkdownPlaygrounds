ebb380c3fe545f2db4abc67a60156698175f89d7
Jonathan Mora <nathanmorax@outlook.com>
2025-07-21 20:47:01 -0600

    Refactor view setup and add file read/write support
    Moved splitView and scrollView configuration to Boilerplate.swift for better code organization. Updated MarkdownDocument to support reading and writing files. Adjusted entitlements to allow read/write access to user-selected and downloads files. Minor UI tweaks to window size and output view constraints.

--- MarkdownPlaygrounds/AppDelegate.swift
+++ MarkdownPlaygrounds/AppDelegate.swift
@@ -32,21 +32,44 @@ class MarkdownDocumentController: NSDocumentController {
     
 }
 
+struct MarkdownError: Error {
+    
+}
+
 @objc(MarkdownDocument)
 class MarkdownDocument: NSDocument {
     
     let contentViewController = ViewController()
     
+    override class var readableTypes: [String] {
+        return ["public.text"]
+    }
+    
+    override class func isNativeType(_ type: String) -> Bool {
+        return true
+    }
+    
+    override func read(from data: Data, ofType typeName: String) throws {
+        guard let str = String(data: data, encoding: .utf8) else  {
+            throw MarkdownError()
+        }
+        contentViewController.editor.string = str
+    }
+    
+    override func data(ofType typeName: String) throws -> Data {
+        contentViewController.editor.breakUndoCoalescing()
+        return contentViewController.editor.string.data(using: .utf8)!
+    }
+    
     override func makeWindowControllers() {
         let window = NSWindow(contentViewController: contentViewController)
-        window.setContentSize(NSSize(width: 800, height: 800))
+        window.setContentSize(NSSize(width: 800, height: 600))
         
         let wc = NSWindowController(window: window)
         wc.contentViewController = contentViewController
         addWindowController(wc)
         
         window.setFrameAutosaveName("windowFrame")
-        
         window.makeKeyAndOrderFront(nil)
         
     }
@@ -62,48 +85,13 @@ final class ViewController: NSViewController {
         
         let editorSV = editor.configureAndWrapInScrollView(isEditable: true, inset: CGSize(width: 30, height: 10))
         let outputSV = output.configureAndWrapInScrollView(isEditable: false, inset: CGSize(width: 10, height: 10))
-        
+        outputSV.widthAnchor.constraint(greaterThanOrEqualToConstant: 200).isActive = true
+
         output.string = "output"
+        editor.allowsUndo = true
         
-        self.view = splitView([editorSV, outputSV])
-        
+        self.view = Boilerplate().splitView([editorSV, outputSV])
         
     }
     
-    func splitView(_ views: [NSView]) -> NSSplitView {
-        let splitView = NSSplitView()
-        splitView.dividerStyle = .thin
-        splitView.isVertical = true
-        splitView.translatesAutoresizingMaskIntoConstraints = false
-
-        for view in views {
-            splitView.addArrangedSubview(view)
-        }
-
-        return splitView
-    }
-
-    
-}
-
-extension NSTextView {
-    func configureAndWrapInScrollView(isEditable: Bool = true, inset: CGSize = .zero) -> NSScrollView {
-        self.isEditable = isEditable
-        self.drawsBackground = true
-        self.backgroundColor = .textBackgroundColor
-        self.isVerticallyResizable = true
-        self.isHorizontallyResizable = false
-        self.textContainerInset = NSSize(width: inset.width, height: inset.height)
-        self.autoresizingMask = [.width]
-        self.textContainer?.widthTracksTextView = true
-
-        let scrollView = NSScrollView()
-        scrollView.hasVerticalScroller = true
-        scrollView.hasHorizontalScroller = false
-        scrollView.documentView = self
-        scrollView.borderType = .noBorder
-        scrollView.autoresizingMask = [.width, .height]
-        
-        return scrollView
-    }
 }
---
+++ MarkdownPlaygrounds/Boilerplate.swift
@@ -0,0 +1,42 @@
+//
+//  Boilerplate.swift
+//  MarkdownPlaygrounds
+//
+//  Created by Jonathan Mora on 21/07/25.
+//
+
+import Cocoa
+
+class Boilerplate {
+    
+    func splitView(_ views: [NSView]) -> NSSplitView {
+        let sv = NSSplitView()
+        sv.isVertical = true
+        sv.dividerStyle = .thin
+        for v in views {
+            sv.addArrangedSubview(v)
+        }
+        sv.setHoldingPriority(.defaultLow - 1, forSubviewAt: 0)
+        sv.autoresizingMask = [.width, .height]
+        sv.autosaveName = "SplitView"
+        return sv
+
+    }
+}
+
+
+
+extension NSTextView {
+    func configureAndWrapInScrollView(isEditable editable: Bool, inset: CGSize) -> NSScrollView {
+        let scrollView = NSScrollView()
+        scrollView.hasVerticalScroller = true
+        
+        isEditable = editable
+        textContainerInset = inset
+        autoresizingMask = [.width]
+        isAutomaticQuoteSubstitutionEnabled = false
+        isAutomaticDashSubstitutionEnabled = false
+        scrollView.documentView = self
+        return scrollView
+    }
+}
