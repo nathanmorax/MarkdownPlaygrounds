6f5dc7cef0b2ed71376daa6acaa7b99fc8b44d90
Jonathan Mora <nathanmorax@outlook.com>
2025-07-21 19:59:42 -0600

    Add split editor and output views to main window
    Introduces a split view in the main window with editable and non-editable NSTextViews for editor and output. Adds a helper extension for configuring and wrapping NSTextViews in scroll views, and enables window frame autosaving.

--- MarkdownPlaygrounds/AppDelegate.swift
+++ MarkdownPlaygrounds/AppDelegate.swift
@@ -45,6 +45,8 @@ class MarkdownDocument: NSDocument {
         wc.contentViewController = contentViewController
         addWindowController(wc)
         
+        window.setFrameAutosaveName("windowFrame")
+        
         window.makeKeyAndOrderFront(nil)
         
     }
@@ -52,10 +54,56 @@ class MarkdownDocument: NSDocument {
 }
 
 final class ViewController: NSViewController {
+    
+    let editor = NSTextView()
+    let output = NSTextView()
+    
     override func loadView() {
-        self.view = NSView()
+        
+        let editorSV = editor.configureAndWrapInScrollView(isEditable: true, inset: CGSize(width: 30, height: 10))
+        let outputSV = output.configureAndWrapInScrollView(isEditable: false, inset: CGSize(width: 10, height: 10))
+        
+        output.string = "output"
+        
+        self.view = splitView([editorSV, outputSV])
         
         
     }
     
+    func splitView(_ views: [NSView]) -> NSSplitView {
+        let splitView = NSSplitView()
+        splitView.dividerStyle = .thin
+        splitView.isVertical = true
+        splitView.translatesAutoresizingMaskIntoConstraints = false
+
+        for view in views {
+            splitView.addArrangedSubview(view)
+        }
+
+        return splitView
+    }
+
+    
+}
+
+extension NSTextView {
+    func configureAndWrapInScrollView(isEditable: Bool = true, inset: CGSize = .zero) -> NSScrollView {
+        self.isEditable = isEditable
+        self.drawsBackground = true
+        self.backgroundColor = .textBackgroundColor
+        self.isVerticallyResizable = true
+        self.isHorizontallyResizable = false
+        self.textContainerInset = NSSize(width: inset.width, height: inset.height)
+        self.autoresizingMask = [.width]
+        self.textContainer?.widthTracksTextView = true
+
+        let scrollView = NSScrollView()
+        scrollView.hasVerticalScroller = true
+        scrollView.hasHorizontalScroller = false
+        scrollView.documentView = self
+        scrollView.borderType = .noBorder
+        scrollView.autoresizingMask = [.width, .height]
+        
+        return scrollView
+    }
 }
