ae89282986c12b8b24864fa3e5f178db069d7f51
Jonathan Mora <nathanmorax@outlook.com>
2025-07-21 19:44:58 -0600

    Implement basic Markdown document handling
    Added MarkdownDocumentController and MarkdownDocument classes to support opening and managing Markdown documents. Introduced a simple ViewController for document windows. Refactored AppDelegate to initialize the document controller on launch.

--- MarkdownPlaygrounds/AppDelegate.swift
+++ MarkdownPlaygrounds/AppDelegate.swift
@@ -10,28 +10,52 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet var window: NSWindow!
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-        return true
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        _ = MarkdownDocumentController()
     }
 
 
 }
 
 class MarkdownDocumentController: NSDocumentController {
+    override var documentClassNames: [String] {
+        return ["MarkdownDocument"]
+    }
+    
+    override var defaultType: String? {
+        return "MarkdownDocument"
+    }
+    
+    override func documentClass(forType typeName: String) -> AnyClass? {
+        return MarkdownDocument.self
+    }
     
 }
 
+@objc(MarkdownDocument)
 class MarkdownDocument: NSDocument {
+    
+    let contentViewController = ViewController()
+    
+    override func makeWindowControllers() {
+        let window = NSWindow(contentViewController: contentViewController)
+        window.setContentSize(NSSize(width: 800, height: 800))
+        
+        let wc = NSWindowController(window: window)
+        wc.contentViewController = contentViewController
+        addWindowController(wc)
+        
+        window.makeKeyAndOrderFront(nil)
+        
+    }
+    
+}
+
+final class ViewController: NSViewController {
+    override func loadView() {
+        self.view = NSView()
+        
+        
+    }
     
 }
